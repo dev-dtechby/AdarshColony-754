11c5b0c46e4fb39b23d753ec8c6144a6c52c2d59
dev-dtechby <devcomputers493551@gmail.com>
2025-12-27 14:13:04 +0530

    fix: resolve the secondry button problem

--- app/[lang]/(dashboard)/(ledger)/fuels-ledger/components/fuelAmountReceive.tsx
+++ app/[lang]/(dashboard)/(ledger)/fuels-ledger/components/fuelAmountReceive.tsx
@@ -103,7 +103,7 @@ export default function FuelAmountReceive({
       {/* ---------- BUTTONS ---------- */}
       <div className="flex flex-wrap gap-3 pt-3">
         <Button className="px-8">Save</Button>
-        <Button variant="secondary" className="px-8">
+        <Button variant="outline" className="px-8">
           Update
         </Button>
         <Button variant="outline" className="px-8">
